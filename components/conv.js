aa319f147ada8381faa9e91519fba87e0234257e
Paul Abib S. Camano <paul.camano@gmail.com>
2022-10-21 20:49:50 +0000

    Implement conversion logic up to 999999

--- components/conv.js
+++ components/conv.js
@@ -1,12 +1,13 @@
 import { useState } from "react"
-import convertToRoman from "../lib/conv";
-import Result from "./result";
+import convertToRoman from "../lib/conv"
+import Result from "./result"
 
 export default function RomanConverter() {
-    const [roman, setRoman] = useState("");
+    const [roman, setRoman] = useState("")
     
     function handleChange(e) {
-        setRoman(() => convertToRoman(e.target.value))
+        Number(e.target.value) > 999999 ?
+        setRoman(() => "") : setRoman(() => convertToRoman(e.target.value))
     }
 
     return (
