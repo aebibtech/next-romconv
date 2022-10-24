bfd7b047e15a5bbd31e8400dcd7ba5377723d07e
Paul Abib S. Camano <paul.camano@gmail.com>
2022-10-24 19:10:24 +0000

    Improveddisplay splay of the overline/vinculum

--- components/result.js
+++ components/result.js
@@ -1,6 +1,12 @@
 import styles from "./result.module.css"
 
 export default function Result({ result }) {
+    // The hack to properly display an overline lol
+    let endBar = result.lastIndexOf("\u0305")
+    let withBar = result.slice(0, endBar + 1).split("").filter(e => e != "\u0305").join("")
+    let woutBar = result.slice(endBar + 1)
+    // End of the hack
+
     function copyToClip() {
         navigator.clipboard.writeText(result)
     }
@@ -8,7 +14,9 @@ export default function Result({ result }) {
     return (
         <>
         <p>
-            <label>{result}</label>
+            <label>
+                <span className={styles.overline}>{withBar}</span>{woutBar}
+            </label>
         </p>
         <p>
             <button onClick={copyToClip} className={result ? "" : styles.btnhide}>Copy to Clipboard</button>
